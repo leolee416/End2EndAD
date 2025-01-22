0adefd5d76ae28f229d9fc0d481c19ba43df9ef1
Zhenjiang Li <zhenjiang1.li@tum.de>
2025-01-22 13:54:21 +0100

    update gitignore; delete default files

--- run.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-if __name__ == '__main__':
-    # subprocess.call(['python', 'train.py', '-d', '/home/yang/git/udacity_test/End-to-End-Learning-for-Self-Driving-Cars-master/data'])
-    subprocess.call(['python', 'drive.py', 'models/model_390000'])
